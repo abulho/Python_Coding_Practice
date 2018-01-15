582a893c3ecebc9b35335beb9d18f035fcfab1db
Abdulla Mohmed <abdulla.aecom@gmail.com>
2018-01-15 11:23:23 -0800

    added more code wars examples

--- code_wars.py
+++ code_wars.py
@@ -32,3 +32,32 @@ def find_uniq(arr):
     sorted_bb = sorted(bb.items(), key=operator.itemgetter(1))
     idx = temp.index(sorted_bb[0][0])
     return arr[idx]
+
+#-----------------------------------------------------------------------------------------------------#
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+'''
+import numpy as np
+def solution(number):
+    '''
+    INPUT:
+        number
+
+    OUTPUT:
+        The sum of all the number divisible by three and five.
+
+    EXAMPLE:
+        solution(10) --> 23
+        passing number 10 into the function should return 23
+    '''
+    nums = np.array(range(1,number))
+    mask3 = nums % 3 == 0
+    mask5 = nums % 5 == 0
+    nums3 = nums[mask3]
+    nums5 = nums[mask5]
+    return sum(list(set((np.append(nums3,nums5)))))
